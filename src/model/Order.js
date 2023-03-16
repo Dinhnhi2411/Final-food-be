fb1d2bbcb33913bd89e55602a8efab96b22cf738
Đinh Ngọc Liên Nhi <dinhngocliennhi1999@gmail.com>
2023-03-16 15:22:31 +0700

    fix order when update price product

--- src/model/Order.js
+++ src/model/Order.js
@@ -3,62 +3,71 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const orderSchema = Schema(
-    {
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    addressShip: {
+      type: String,
+      required: true,
+    },
+    products: [
+      {
 
-    products: [{
-        product: {
-            type: Schema.Types.ObjectId,
-            required: true,
-            ref: "Product",
+        product:{
+          type: Schema.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+        price: {
+          type: Number,
+          required: true,
         },
         amount: {
-            type: Number,
-            required: true,
+          type: Number,
+          required: true,
         },
         sum: {
-            type: Number, 
-            required: true,
+          type: Number,
+          required: true,
         },
-        createAt: { type: Date},
+        createAt: { type: Date },
         updatedAt: { type: Date },
-
-    }],
+      },
+    ],
     priceShip: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
+
     total: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     userId: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: "User",
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
     status: {
-        type: String,
-        enum: [
-            "Preparing Order",
-            "Order is shipping",
-            "Delivered",
-            "Cancel"
-
-        ],
-        default: "Preparing Order",
-        required: true,
+      type: String,
+      enum: ["Preparing Order", "Order is shipping", "Delivered", "Cancel"],
+      default: "Preparing Order",
+      required: true,
     },
     isDeleted: {
-        type: Boolean, 
-        default: false,
-        select: false,
+      type: Boolean,
+      default: false,
+      select: false,
     },
-
-    }, 
-    { timestamps: true }
+  },
+  { timestamps: true }
 );
-
-
 
 const Order = mongoose.model("Order", orderSchema);
 module.exports = Order;
